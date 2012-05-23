18c515592b6888ec3fdcdea25a3c2cff0677930f
Jacek <jacek@kuzemczak.co.uk>
2012-05-23 14:23:27 +0100

    Added logging and config things :)

--- uk/co/jacekk/bukkit/baseplugin/BasePlugin.java
+++ uk/co/jacekk/bukkit/baseplugin/BasePlugin.java
@@ -4,12 +4,18 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import uk.co.jacekk.bukkit.baseplugin.util.PluginLogger;
+
 public abstract class BasePlugin extends JavaPlugin {
 	
 	protected PluginDescriptionFile description;
 	
+	protected PluginLogger log;
+	
 	public void onEnable(){
 		this.description = this.getDescription();
+		
+		this.log = new PluginLogger(this);
 	}
 	
 	public String formatMessage(String message, boolean colour, boolean version){
---
+++ uk/co/jacekk/bukkit/baseplugin/util/PluginConfigKey.java
@@ -0,0 +1,9 @@
+package uk.co.jacekk.bukkit.baseplugin.util;
+
+public interface PluginConfigKey {
+	
+	public String getKey();
+	
+	public Object getDefault();
+	
+}
