4f4edffdcfdb08ca5b6c039f7de9e2dbf6a5a838
Jacek <jacek@kuzemczak.co.uk>
2012-05-23 15:13:35 +0100

    Added a config field, still has to be defined by the main pluign

--- uk/co/jacekk/bukkit/baseplugin/BasePlugin.java
+++ uk/co/jacekk/bukkit/baseplugin/BasePlugin.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import uk.co.jacekk.bukkit.baseplugin.config.PluginConfig;
 import uk.co.jacekk.bukkit.baseplugin.util.PluginLogger;
 
 public abstract class BasePlugin extends JavaPlugin {
@@ -16,6 +17,8 @@ public abstract class BasePlugin extends JavaPlugin {
 	protected PluginDescriptionFile description;
 	
 	protected PluginLogger log;
+	
+	protected PluginConfig config;
 	
 	protected Server server;
 	protected PluginManager pluginManager;
