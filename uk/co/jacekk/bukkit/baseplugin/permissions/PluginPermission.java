eb788aaaa1d240e4ce52a2260b4a06e7aa77cb1f
Jacek <jacek@kuzemczak.co.uk>
2012-07-10 15:32:25 +0100

    We know its a permission ...]

--- uk/co/jacekk/bukkit/baseplugin/permissions/PluginPermission.java
+++ uk/co/jacekk/bukkit/baseplugin/permissions/PluginPermission.java
@@ -8,14 +8,14 @@ import org.bukkit.permissions.PermissionDefault;
 
 public interface PluginPermission {
 	
-	public List<Player> getPlayersWith();
-	
-	public Boolean hasPermission(CommandSender sender);
-	
 	public String getNode();
 	
 	public PermissionDefault getDefault();
 	
 	public String getDescription();
+	
+	public List<Player> getPlayersWith();
+	
+	public Boolean has(CommandSender sender);
 	
 }
