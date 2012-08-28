9207e6a53b454e3135e60c2274b9c93c9d72cc9b
Jacek <jacek@kuzemczak.co.uk>
2012-08-28 15:42:59 +0100

    Split the update checker into two classes to allow for adding custom version number sources.

--- src/main/java/uk/co/jacekk/bukkit/baseplugin/update/BukkitDevUpdateChecker.java
+++ src/main/java/uk/co/jacekk/bukkit/baseplugin/update/BukkitDevUpdateChecker.java
@@ -3,9 +3,7 @@ package uk.co.jacekk.bukkit.baseplugin.update;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -14,74 +12,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class BukkitDevUpdateChecker {
-	
-	private Plugin plugin;
-	private URL filesFeed;
-	
-	private String version;
-	private String link;
-	private String jarLink;
-	
-	private HashMap<String, Integer> stringValues;
+public class BukkitDevUpdateChecker extends UpdateChecker {
 	
 	public BukkitDevUpdateChecker(Plugin plugin, String url){
-		this.plugin = plugin;
-		
-		try{
-			this.filesFeed = new URL(url);
-		}catch (MalformedURLException e){
-			e.printStackTrace();
-		}
-		
-		this.stringValues = new HashMap<String, Integer>();
-		
-		this.stringValues.put("dev", -1);
-		this.stringValues.put("alpha", 1);
-		this.stringValues.put("beta", 2);
-		this.stringValues.put("release", 3);
-	}
-	
-	private int numericValueOf(String part){
-		Integer value = 0;
-		
-		try{
-			value = Integer.parseInt(part);
-		}catch (NumberFormatException e){
-			if (part.length() == 1){
-				return part.getBytes()[0];
-			}else if (this.stringValues.containsKey(part)){
-				return this.stringValues.get(part);
-			}
-			
-			return 0;
-		}
-		
-		return value;
-	}
-	
-	private int versionCompare(String a, String b){
-		if (a.equalsIgnoreCase(b)){
-			return 0;
-		}
-		
-		String[] partsa = a.toLowerCase().replaceAll("([0-9]+)([a-z]+)", "$1.$2").replaceAll("([a-z]+)([0-9]+)", "$1.$2").split("[_ ,.+]{1}");
-		String[] partsb = b.toLowerCase().replaceAll("([0-9]+)([a-z]+)", "$1.$2").replaceAll("([a-z]+)([0-9]+)", "$1.$2").split("[_ ,.+]{1}");
-		
-		int max = Math.max(partsa.length, partsb.length);
-		
-		for (int i = 0; i < max; ++i){
-			Integer vala = (i < partsa.length) ? this.numericValueOf(partsa[i]) : 0;
-			Integer valb = (i < partsb.length) ? this.numericValueOf(partsb[i]) : 0;
-			
-			if (vala > valb){
-				return 1;
-			}else if (vala < valb){
-				return -1;
-			}
-		}
-		
-		return -1;
+		super(plugin, url);
 	}
 	
 	public boolean updateNeeded(){
@@ -121,18 +55,6 @@ public class BukkitDevUpdateChecker {
 		}
 		
 		return false;
-	}
-	
-	public String getVersion(){
-		return this.version;
-	}
-	
-	public String getLink(){
-		return this.link;
-	}
-	
-	public String getJarLink(){
-		return this.jarLink;
 	}
 	
 }
