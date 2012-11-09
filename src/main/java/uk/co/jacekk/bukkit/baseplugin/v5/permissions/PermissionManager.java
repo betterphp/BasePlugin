52fb6f509c1b75c42940e96e90e9c29c5f88b662
Jacek <jacek@kuzemczak.co.uk>
2012-11-09 23:02:45 +0000

    Simpler permissions system.

--- src/main/java/uk/co/jacekk/bukkit/baseplugin/v5/permissions/PermissionManager.java
+++ src/main/java/uk/co/jacekk/bukkit/baseplugin/v5/permissions/PermissionManager.java
@@ -1,5 +1,9 @@
 package uk.co.jacekk.bukkit.baseplugin.v5.permissions;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.permissions.Permission;
 
 import uk.co.jacekk.bukkit.baseplugin.v5.BaseObject;
@@ -20,6 +24,27 @@ public class PermissionManager extends BaseObject<BasePlugin> {
 		for (PluginPermission permission : permissions){
 			plugin.pluginManager.addPermission(new Permission(permission.getNode(), permission.getDescription(), permission.getDefault()));
 		}
+	}
+	
+	/**
+	 * Registers the default permissions for a plugin
+	 * 
+	 * @param permissionHolder	The class holding all of the {@link PluginPermission} objects.
+	 */
+	public void registerPermissions(Class<?> permissionHolder){
+		List<PluginPermission> perms = Arrays.asList();
+		
+		for (Field field : permissionHolder.getDeclaredFields()){
+			if (field.getType().equals(PluginPermission.class)){
+				try{
+					perms.add((PluginPermission) field.get(null));
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		this.registerPermissions(perms.toArray(new PluginPermission[0]));
 	}
 	
 }
--- src/main/java/uk/co/jacekk/bukkit/baseplugin/v5/permissions/PluginPermission.java
+++ src/main/java/uk/co/jacekk/bukkit/baseplugin/v5/permissions/PluginPermission.java
@@ -1,7 +1,9 @@
 package uk.co.jacekk.bukkit.baseplugin.v5.permissions;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
@@ -11,35 +13,61 @@ import org.bukkit.permissions.PermissionDefault;
  * 
  * @author Jacek Kuzemczak
  */
-public interface PluginPermission {
+public class PluginPermission {
+	
+	protected String node;
+	protected PermissionDefault defaultValue;
+	protected String description;
+	
+	public PluginPermission(String node, PermissionDefault defaultValue, String description){
+		this.node = node;
+		this.defaultValue = defaultValue;
+		this.description = description;
+	}
 	
 	/**
 	 * Gets the node for this permission.
 	 * 
 	 * @return	The node.
 	 */
-	public String getNode();
+	public String getNode(){
+		return this.node;
+	}
 	
 	/**
 	 * Gets the {@link PermissionDefault} for this permission.
 	 * 
 	 * @return The default.
 	 */
-	public PermissionDefault getDefault();
+	public PermissionDefault getDefault(){
+		return this.defaultValue;
+	}
 	
 	/**
 	 * Gets a short description of what this permission allows.
 	 * 
 	 * @return The description.
 	 */
-	public String getDescription();
+	public String getDescription(){
+		return this.description;
+	}
 	
 	/**
 	 * Gets a list of all players on the server with this permission.
 	 * 
 	 * @return The list of players.
 	 */
-	public List<Player> getPlayersWith();
+	public List<Player> getPlayersWith(){
+		ArrayList<Player> players = new ArrayList<Player>();
+		
+		for (Player player : Bukkit.getServer().getOnlinePlayers()){
+			if (this.has(player)){
+				players.add(player);
+			}
+		}
+		
+		return players;
+	}
 	
 	/**
 	 * Checks to see if a {@link CommandSender} has this permission.
@@ -47,6 +75,8 @@ public interface PluginPermission {
 	 * @param sender	The sender to be checked, often a {@link Player}
 	 * @return			True if the sender has this permission false if not.
 	 */
-	public boolean has(CommandSender sender);
+	public boolean has(CommandSender sender){
+		return sender.hasPermission(this.node);
+	}
 	
 }
